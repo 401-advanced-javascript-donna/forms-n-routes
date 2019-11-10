34c1d1f6b66feeffcfbee4fd117f1dcd1a6aae00
sanvean74 <dlambertpdx@gmail.com>
2019-11-09 18:43:50 -0800

    more tests passing

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Form = ({ form, handleSubmit, handleChange }) => {
+const Form = ({ search, handleSubmit, handleChange }) => {
 
   return (
     <form onSubmit={handleSubmit}>
       <h2>Find a Character</h2>
-      <input type="text" name="form" value={form} onChange={handleChange}></input>
+      <input type="text" name="search" value={search} onChange={handleChange}></input>
       <button>Form</button>
     </form>
   );
 };
 
 Form.propTypes = {
-  form: PropTypes.string.isRequired,
+  search: PropTypes.string.isRequired,
   handleSubmit: PropTypes.func.isRequired,
   handleChange: PropTypes.func.isRequired
 };
